f2a10e571ca671bfa5e152bd30f9c073c43d553c
Alexandre Bour <ayorosmage@gmail.com>
2014-07-05 16:49:52 +0200

    Improve Player display feedback according speed

--- PlayerDisplayer.js
+++ PlayerDisplayer.js
@@ -5,11 +5,8 @@ exports.createDisplayerFunc = function(networkCommunicationManager) {
     return function(p, horizontalSpeed) {
         var millisecTime = new Date().getTime();
 
-        // var subCircleX = p.getRayon() * 0.8 * Math.cos((millisecTime % 36000 * 1000) * (horizontalSpeed / 5) / -3000000000.);
-        // var subCircleY = p.getRayon() * 0.8 * Math.sin((millisecTime % 36000 * 1000) * (horizontalSpeed / 5) / -3000000000.);
-
-        var subCircleX = p.getRayon() * 0.8 * Math.cos((millisecTime % 36000 * 1000) * (horizontalSpeed / 5) / -3000000.);
-        var subCircleY = p.getRayon() * 0.8 * Math.sin((millisecTime % 36000 * 1000) * (horizontalSpeed / 5) / -3000000.);
+        var subCircleX = p.getRayon() * 0.8 * Math.cos((millisecTime % 36000 * 1000) * (horizontalSpeed) / -10000000.);
+        var subCircleY = p.getRayon() * 0.8 * Math.sin((millisecTime % 36000 * 1000) * (horizontalSpeed) / -10000000.);
 
         networkCommunicationManager.drawCircle(p.getX() + subCircleX, p.getY() +  subCircleY, p.getRayon() / 2, 6);
         networkCommunicationManager.drawCircle(p.getX(), p.getY(), p.getRayon() * 2, 6);
